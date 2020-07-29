d4f70d4049fe33f5d5dd5be729159a62f5de8c78
Eugene <NoskovEugene@gmail.com>
2020-07-29 13:26:56 +0300

    Initial Commit

---
+++ ConsoleInvoker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleInvoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
