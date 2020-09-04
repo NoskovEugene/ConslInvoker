1f7ef4503f1484f5b1fa469624c4eddbc30d5e88
Eugene <NoskovEugene@gmail.com>
2020-09-04 15:07:49 +0300

    Now messenger will be divide message

--- Core/Analyzers/Analyzer.cs
+++ Core/Analyzers/Analyzer.cs
@@ -13,8 +13,9 @@ namespace Core.Analyzers
         public Package Analyze(Package package)
         {
             var array = package.UnparsedString.Split(StringSplitOptions.RemoveEmptyEntries, " ");
-            if(array.Length == 0)
-                throw new Exception("Input line cannot be empty");
+            if(array.Length == 0){
+                throw new ArgumentNullException("Input line cannot be empty");
+            }
             package.Command = array[0];
             array = array.Where(x=> x != package.Command).ToArray();
             package.Parameter = string.Join(" ", array);
